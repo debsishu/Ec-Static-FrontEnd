d140ee54d8f83ffd959af5ab2b6770bd8684957b
Debsishu Pal <pauldebsishu2002@gmail.com>
2022-09-14 22:07:54 +0530

    Default route add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
