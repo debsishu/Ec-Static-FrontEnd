7085135d8d9d44673237a2c2134f1a12d9cb25e0
Prasanta Acharyya <acharyya.prasanta05@gmail.com>
2022-09-14 22:14:52 +0530

    added Login and Profile pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
